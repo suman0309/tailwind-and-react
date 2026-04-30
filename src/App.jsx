04267d32cfafbf56e5f63a83cceeec34f63e1519
suman0309 <sumanmondal0612@gmail.com>
2026-04-30 18:12:13 +0530

    tailwind concured

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import  Card from './components/Card'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+let myObj={
+  usernanme:"hitesh",
+  age:21
+}
   return (
     <>
-     <h1 className='bg-green-500'>hii tailwind tets</h1>
+    {/* // channel,someObj these are known as props that are send to to card.jsx and it uses these as it properties   */}
+    {/* //function card(props){
+    console.log("props",props.username)
+    is equivalent to 
+    function card({username}){
+    console.log(username)
+    basically they can be used as variables
+    
+    */}
+    <h1 className='bg-green-400'>Tailwindcss</h1>
+    <Card username="chaiaurcodee" btnText="click me" />
+    <Card username="heheh"/>
+    
     </>
   )
 }
